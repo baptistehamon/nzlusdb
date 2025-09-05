19a9f760f51a17eeb2abde8e16bec490d88753a8
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-09-05 13:46:29 +1200

    Fix rootpath

--- src/nzlusdb/__init__.py
+++ src/nzlusdb/__init__.py
@@ -6,4 +6,6 @@ __author__ = "Baptiste Hamon"
 __email__ = "baptiste.hamon@pg.canterbury.ac.nz"
 __version__ = "0.0-dev2"
 
-ROOTPATH = Path(r"r'R:\DATA\NZLUSDB")
+__all__ = ["ROOTPATH"]
+
+ROOTPATH = Path(r"R:\DATA\NZLUSDB")
