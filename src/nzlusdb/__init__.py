ade2fb1f153b4c26264558da2adb018497f2311b
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-09-05 13:16:48 +1200

    Add database rootpath

--- src/nzlusdb/__init__.py
+++ src/nzlusdb/__init__.py
@@ -1,5 +1,9 @@
 """Python Code of New Zealand Land Use Database (NZLUSDB)."""
 
+from pathlib import Path
+
 __author__ = "Baptiste Hamon"
 __email__ = "baptiste.hamon@pg.canterbury.ac.nz"
 __version__ = "0.0-dev2"
+
+ROOTPATH = Path(r"r'R:\DATA\NZLUSDB")
