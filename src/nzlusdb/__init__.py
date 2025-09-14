1cef3d1ba7cd25800ced1d59bc5c1c1da43654e8
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2025-09-14 15:30:08 +1200

    Bump version: 0.0-dev3 → 0.0-dev4
    Signed-off-by: Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>

--- src/nzlusdb/__init__.py
+++ src/nzlusdb/__init__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 __author__ = "Baptiste Hamon"
 __email__ = "baptiste.hamon@pg.canterbury.ac.nz"
-__version__ = "0.0-dev3"
+__version__ = "0.0-dev4"
 
 __all__ = ["ROOTPATH"]
 
