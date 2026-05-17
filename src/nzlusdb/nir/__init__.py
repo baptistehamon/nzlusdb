9b6af37c9c9a0b6e09fc24087f581e61b3ee608f
Baptiste Hamon <baptiste.hamon@pg.canterbury.ac.nz>
2026-05-18 11:35:19 +1200

    Add __init__ to nir module

---
+++ src/nzlusdb/nir/__init__.py
@@ -0,0 +1,15 @@
+"""Module defining NIR parameters."""
+
+import importlib
+import pkgutil
+
+__all__ = []
+
+# Dynamically import all modules in this package
+for _loader, module_name, _is_pkg in pkgutil.iter_modules(__path__):
+    module = importlib.import_module(f"{__name__}.{module_name}")
+    if hasattr(module, "__all__"):
+        # Import all symbols listed in __all__ of the module
+        for symbol in module.__all__:
+            globals()[symbol] = getattr(module, symbol)
+        __all__.extend(module.__all__)
